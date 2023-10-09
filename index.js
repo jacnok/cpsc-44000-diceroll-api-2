7b709dc839b7209e88b1fd92a5570d71ae294c9d
Jefferson C <jcherton@hotmail.com>
2023-10-09 05:55:14 -0500

    let's find out if this works

--- index.js
+++ index.js
@@ -11,120 +11,68 @@ const minorVersion = 2
 // Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
 app.use(express.static(__dirname + '/static'))
 
-// The app.get functions below are being processed in Node.js running on the server.
-// Implement a custom About page.
-app.get('/about', (request, response) => {
-	console.log('Calling "/about" on the Node.js server.')
-	response.type('text/plain')
-	response.send('About Node.js on Azure Template.')
-})
+function randGen()
+  {
+    return (Math.floor(Math.random() * 6)) + 1;
+  }
 
-app.get('/version', (request, response) => {
-	console.log('Calling "/version" on the Node.js server.')
-	response.type('text/plain')
-	response.send('Version: '+majorVersion+'.'+minorVersion)
-})
-
-// Return the value of 2 plus 2.
-app.get('/2plus2', (request, response) => {
-	console.log('Calling "/2plus2" on the Node.js server.')
-	response.type('text/plain')
-	response.send('4')
-})
-
-// Add x and y which are both passed in on the URL. 
-app.get('/add-two-integers', (request, response) => {
-	console.log('Calling "/add-two-integers" on the Node.js server.')
-	var inputs = url.parse(request.url, true).query
-	let x = parseInt(inputs.x)
-	let y = parseInt(inputs.y)
-	let sum = x + y
-	response.type('text/plain')
-	response.send(sum.toString())
-})
-
-// Template for calculating BMI using height in feet/inches and weight in pounds.
-app.get('/calculate-bmi', (request, response) => {
-	console.log('Calling "/calculate-bmi" on the Node.js server.')
-	var inputs = url.parse(request.url, true).query
-	const heightFeet = parseInt(inputs.feet)
-	const heightInches = parseInt(inputs.inches)
-	const weight = parseInt(inputs.lbs)
-
-	console.log('Height:' + heightFeet + '\'' + heightInches + '\"')
-	console.log('Weight:' + weight + ' lbs.')
-
-	// Todo: Implement unit conversions and BMI calculations.
-	// Todo: Return BMI instead of Todo message.
-
-	response.type('text/plain')
-	response.send('Todo: Implement "/calculate-bmi"')
-})
-
-// Test a variety of functions.
-app.get('/test', (request, response) => {
-    // Write the request to the log. 
-    console.log(request);
-
-    // Return HTML.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Testing Function</h3>')
-
-    // Access function from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
-
-    // Show the full url from the request. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
+app.get("/api/v1", (req, res) => {
     
-	// Parse the query string for values that are being passed on the URL.
-	var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
+    // const allDicerolls = dicerollService.getAllDicerolls();
+    // res.send({ status: "OK", data: allDicerolls });
 
-    // Close the response
-    response.end('<h3>The End.</h3>');
-})
+    const { body } = req;
 
-// Return Batman as JSON.
-var spiderMan = {
-	"firstName":"Bruce",
-	"lastName":"Wayne",
-	"preferredName":"Batman",
-	"email":"darkknight@lewisu.edu",
-	"phoneNumber":"800-bat-mann",
-	"city":"Gotham",
-	"state":"NJ",
-	"zip":"07101",
-	"lat":"40.73",
-	"lng":"-74.17",
-	"favoriteHobby":"Flying",
-	"class":"cpsc-24700-001",
-	"room":"AS-104-A",
-	"startTime":"2 PM CT",
-	"seatNumber":"",
-	"inPerson":[
-		"Monday",
-		"Wednesday"
-	],
-	"virtual":[
-		"Friday"
-	]
-}
+    let roller = [0,0,0,0,0,0]
 
-app.get('/batman', (request, response) => {
-	console.log('Calling "/batman" on the Node.js server.')
-	response.type('application/json')
-	response.send(JSON.stringify(spiderMan, null, 4))
-})
+
+    body[1] == "true" ? roller[1] = randGen() : roller[1] = 0;
+    body[2] == "true" ? roller[2] = randGen() : roller[2] = 0;
+    body[3] == "true" ? roller[3] = randGen() : roller[3] = 0;
+    body[4] == "true" ? roller[4] = randGen() : roller[4] = 0;
+    body[5] == "true" ? roller[5] = randGen() : roller[5] = 0;
+
+    res.json(roller);
+
+    return;
+
+    
+  });
+
+  app.get("/api/v1/test", (req, res) => { 
+
+    let roller = [0,0,0,0,0,0]
+
+    roller[1] = randGen() * Math.pow(randGen(), 2);
+    roller[2] = randGen() * Math.pow(randGen(), 2);
+    roller[3] = randGen() * Math.pow(randGen(), 2);
+    roller[4] = randGen() * Math.pow(randGen(), 2);
+    roller[5] = randGen() * Math.pow(randGen(), 2);
+
+    avg = (roller[1] + roller[2] + roller[3] + roller[4] + roller[5]) / 5;
+
+    msg = "This is only testing part of the capabilities of my API: <br>";
+
+    msg = msg + "("
+          + roller[1].toString() + " + "
+          + roller[2].toString() + " + "
+          + roller[3].toString() + " + "
+          + roller[4].toString() + " + "
+          + roller[5].toString() + ") / 5 = "
+          + "<b>" + avg.toString() + "</b>";
+
+    // send is natively HTML code.
+    res.send(msg);
+  });
+
+
+
 
 // Custom 404 page.
 app.use((request, response) => {
   response.type('text/plain')
   response.status(404)
-  response.send('404 - Not Found')
+  response.send('404 - Not Really Found')
 })
 
 // Custom 500 page.
@@ -132,7 +80,7 @@ app.use((err, request, response, next) => {
   console.error(err.message)
   response.type('text/plain')
   response.status(500)
-  response.send('500 - Server Error')
+  response.send('500 - Server Is Now An Error')
 })
 
 app.listen(port, () => console.log(
